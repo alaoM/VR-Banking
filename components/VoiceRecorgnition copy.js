56d5942705b38b48a0c1b4ac4081255e227e8aab
Miracle Alao <71924269+alaoM@users.noreply.github.com>
2023-06-07 06:32:40 +0100

    VR

--- components/VoiceRecorgnition copy.js
+++ components/VoiceRecorgnition copy.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 import AudioVisuals from "./AudioVisuals";
+import { useRouter } from "next/router";
 
 const VoiceRecognition = ({ balanceState }) => {
   const [message, setMessage] = useState("");
@@ -70,61 +70,8 @@ const VoiceRecognition = ({ balanceState }) => {
   useEffect(() => {
     if (transcript !== "") {
       console.log("Transcript:", transcript);
-      processTranscript(transcript);
     }
   }, [transcript]);
-
-  const processTranscript = (transcript) => {
-    const sentences = transcript.split("."); // Split transcript into individual sentences
-
-    sentences.forEach((sentence) => {
-      const sanitizedSentence = sentence.trim().toLowerCase();
-      if (sanitizedSentence !== "") {
-        processVoiceCommand(sanitizedSentence);
-      }
-    });
-  };
-
-  const processVoiceCommand = (command) => {
-    // Modify the logic based on the recognized voice command
-    if (command === "reset") {
-      resetTranscript();
-    } else if (command === "transfer") {
-      router.push("/transfer");
-    } else if (command === "go back" || command === "home") {
-      router.push("/");
-    } else if (command === "shut up") {
-      setMessage("I wasn't talking.");
-    } else if (command === "hello") {
-      setMessage("Hi there!");
-    } else if (
-      [
-        "get balance",
-        "my balance",
-        "account balance",
-        "display balance",
-        "reveal balance",
-        "show balance",
-        "display my balance",
-        "reveal my balance",
-        "show my balance",
-        "how much is my balance",
-      ].includes(command)
-    ) {
-      setMessage("Show Balance");
-      handleBalance(true);
-    } else if (
-      [
-        "hide balance",
-        "hide my balance",
-        "close my balance",
-        "close balance",
-      ].includes(command)
-    ) {
-      setMessage("Hide Balance");
-      handleBalance(false);
-    }
-  };
 
   const handleBalance = (showBalance) => {
     balanceState(showBalance);
@@ -140,14 +87,14 @@ const VoiceRecognition = ({ balanceState }) => {
 
   const stopListening = () => {
     SpeechRecognition.stopListening();
-    resetTranscript();
+    resetTranscript()
     setListen(false);
   };
 
   return (
     <div>
-      <p>Message: {message}</p>
-      <p>Transcript: {transcript}</p>
+    <p>Message: {message}</p> 
+      <p>Transcript: {transcript}</p> 
       <div className="card-footer bg-transparent border-top px-md-5 text-center">
         <p> {listening && <AudioVisuals />}</p>
         <div className="d-flex justify-content-center align-items-center">
@@ -167,6 +114,8 @@ const VoiceRecognition = ({ balanceState }) => {
             </div>
           )}
         </div>
+
+         
       </div>
     </div>
   );
